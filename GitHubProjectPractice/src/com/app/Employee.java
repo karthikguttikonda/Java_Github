45ec02e00dcdeb83fd191d7d7d035181c59010fb
guttikondakarthik <guttikondakarthik555@gmail.com>
2022-09-10 15:43:09 +0530

    GitHubProjectPractice Update

---
+++ GitHubProjectPractice/src/com/app/Employee.java
@@ -0,0 +1,17 @@
+package com.app;
+
+public class Employee {
+	public static void main(String[] args) {    
+        String string = "The best of worlds";    
+        int count = 0;    
+            
+        //Counts each character except space    
+        for(int i = 0; i < string.length(); i++) {    
+            if(string.charAt(i) != ' ')    
+                count++;    
+        }    
+            
+        //Displays the total number of characters present in the given string    
+        System.out.println("Total characters in a string: " + count);    
+    }    
+}
